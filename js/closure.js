1e156e4f39ee4bac249b14c11428ee53c45675e6
JunyeongYang <yang@junyeong.kr>
2019-08-13 22:20:43 +0900

    closure IIFE

--- js/closure.js
+++ js/closure.js
@@ -23,3 +23,35 @@ const vueData = getSet()
 console.log(vueData.getID())
 vueData.setID(8959)
 console.log(vueData.getID())
+
+
+//IIFE
+function iife(userList) {
+  let userID = 100
+  for(let i = 0; i < userList.length; i++) {
+    //즉시 호출된 함수 표현식(Immediately Invoked Function Expression. IIFE)
+    userList[i]["id"] = ((j) => {
+      return userID + j
+    })(i)
+  }
+
+  return userList
+}
+
+let userList = [
+  {
+    id: '',
+    name: '토르'
+  },
+  {
+    id: '',
+    name: '아이언맨'
+  },
+  {
+    id: '',
+    name: '캡틴아메리카'
+  }
+]
+
+let t = iife(userList)
+console.log(t)
