90c56f067a5e0779ac40612b0243178cf1f3efe6
JunyeongYang <yang@junyeong.kr>
2019-08-21 21:37:04 +0900

    js IIFE

---
+++ js/IIFE.js
@@ -0,0 +1,20 @@
+// var a = 2;
+
+// (function IIFE(def) {
+//   def(window)
+// })(function def(global) {
+//   var a = 3
+//   console.log(a)
+//   console.log(global.a)
+// })
+
+const a = () => {
+  var i
+  for(i = 0; i < 10; i++) {
+    ((a) => {
+      console.log(a)
+    })(i)
+  }
+}
+
+a()
