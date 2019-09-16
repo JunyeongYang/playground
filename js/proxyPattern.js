ee612fb45e6a534b6615d96d4b4603868e5a5a21
JunyeongYang <yang@junyeong.kr>
2019-09-16 18:15:06 +0900

    proxy pattern

--- js/proxyPattern.js
+++ js/proxyPattern.js
@@ -36,25 +36,60 @@ function PhoneBookProxy() {
   }
 }
 
-const phoneBookProxy = new PhoneBookProxy()
+// const phoneBookProxy = new PhoneBookProxy()
 
-phoneBookProxy.get('austin', (phoneNumber) => {
-  console.log(phoneNumber)
-})
-console.log(phoneBookProxy.getViewCount())
-
-setTimeout(()=> {
-  phoneBookProxy.get('austin', (phoneNumber) => {
-    console.log(phoneNumber)
-  })
-  phoneBookProxy.get('austin', (phoneNumber) => {
-    console.log(phoneNumber)
-  })
-  console.log(phoneBookProxy.getViewCount())
-}, 5000)
-
-// const phoneBook = new PhoneBook()
-
-// phoneBook.get('austin', (phoneNumber) => {
+// phoneBookProxy.get('austin', (phoneNumber) => {
 //   console.log(phoneNumber)
 // })
+// console.log(phoneBookProxy.getViewCount())
+
+// setTimeout(()=> {
+//   phoneBookProxy.get('austin', (phoneNumber) => {
+//     console.log(phoneNumber)
+//   })
+//   phoneBookProxy.get('austin', (phoneNumber) => {
+//     console.log(phoneNumber)
+//   })
+//   console.log(phoneBookProxy.getViewCount())
+// }, 5000)
+
+
+
+function Phonebook2() {
+  this.dictionary = [
+    {name: 'Austin', pNo: '000-0000-0000'},
+    {name: 'Billie', pNo: '111-1111-1111'}
+  ]
+}
+
+Phonebook2.prototype.get = function(name, callback) {
+  callback(this.dictionary.filter(info => info.name === name))
+}
+
+
+function proxyPhonebook2(){
+  const phonebook2 = new Phonebook2()
+  const cache = {}
+
+  return {
+    get: (name) => {
+      if(!cache[name]) {
+        console.log('not cached');
+        phonebook2.get(name, (res) => {
+          if(res[0])
+            cache[name] = res[0]
+        })
+      }
+      return cache[name]
+    }
+  }
+}
+
+const pp2 = new proxyPhonebook2()
+
+console.log(pp2.get('d'))
+console.log(pp2.get('Austin'));
+console.log(pp2.get('Austin'));
+console.log(pp2.get('Billie'));
+console.log(pp2.get('Austin'));
+console.log(pp2.get('Billie'));
